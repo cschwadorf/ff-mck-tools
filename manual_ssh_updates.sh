c8828b2b4344ca0f8f808c6b5b514a9a0953f0b8
Christoph Schwadorf <schwadorf@it-service-schwadorf.de>
2018-02-05 21:10:32 +0100

    UCI BEfehle fuer manuelle setzen des Firmware Updateservers mit Autoupdater

---
+++ manual_ssh_updates.sh
@@ -0,0 +1,7 @@
+uci add_list autoupdater.stable.mirror='http://[fda0:747e:ab29:2241:215:5dff:fee1:d02f]/update/stable/images/sysupgrade'
+uci set autoupdater.stable.good_signatures='1'
+uci set autoupdater.stable.pubkey=''
+uci add_list autoupdater.stable.pubkey='f91c4445e69fbc9cdebbe1015dc8b9ad996046c06280e4b60190d2f2279182cb'
+uci set autoupdater.settings.enabled='1'
+uci set autoupdater.settings.branch='stable'
+uci commit autoupdater
